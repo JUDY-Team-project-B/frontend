0b9b84b71d6cfffae165592386cec41549f67d8c
BellYun <95991290+BellYun@users.noreply.github.com>
2023-05-15 18:21:39 +0900

    feat : svg 파일 input

--- src/components/main/mappage.tsx
+++ src/components/main/mappage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './mappage.scss';
+import { ReactComponent as Map } from '@/assets/image/southKoreaLow.svg';
 
 const Mappage = () => {
   return (
     <div className="mapPageBackground">
       <div>여행가고 싶은곳을 눌러보세요</div>
-      <div>mappage</div>
+      <Map className="mapdetail" />
     </div>
   );
 };
