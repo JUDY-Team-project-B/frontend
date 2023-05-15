9abb729a2d14a2ff5d93dec21a5f503f1cddf7ee
BellYun <95991290+BellYun@users.noreply.github.com>
2023-05-15 16:31:47 +0900

    feat : nav및 메인 페이지 레이아웃 설정

---
+++ src/components/main/mappage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './mappage.scss';
+
+const Mappage = () => {
+  return (
+    <div className="mapPageBackground">
+      <div>여행가고 싶은곳을 눌러보세요</div>
+      <div>mappage</div>
+    </div>
+  );
+};
+
+export default Mappage;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './index.scss';
-import { useRecoilValue } from 'recoil';
+import Mappage from '@/components/main/mappage';
+import Nav from '@/components/common/nav';
 
 const Main = () => {
-  return <div>main pages</div>;
+  return (
+    <div className="mainBackground">
+      <Nav></Nav>
+      <Mappage></Mappage>
+      <div>contextpage</div>
+    </div>
+  );
 };
 
 export default Main;
