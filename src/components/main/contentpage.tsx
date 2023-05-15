40cd01bada783c22ed679313c52a01142488be3a
BellYun <whd3558@naver.com>
2023-05-15 23:52:54 +0900

    post 미리보기 레이아웃설정

--- src/components/main/contentpage.tsx
+++ src/components/main/contentpage.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './contentpage.scss';
+import img from "@/assets/image/user.png" 
 
 const Contentpage = () => {
   return (
-    <div className="contentpageBackground">
-      <div>곧 출발하는 여행</div>
-      <div>결과</div>
+    <div className="contentBackground"> 
+      <div className='content'> 
+        <div className='img'> 
+          <div className='destiantion'> 
+            제주 
+          </div> 
+          <div className='name'> 
+            하트 
+          </div> 
+        </div> 
+        <div> 
+          조회수 
+        </div> 
+        <div> 
+          <div> 
+            title 
+          </div> 
+          <div> 
+            date 
+          </div> 
+          <div> 
+            how 
+          </div> 
+          <div> 
+            need 
+          </div>        
+        </div> 
+      </div> 
     </div>
   );
 };
---
+++ src/components/main/map.tsx
@@ -0,0 +1,13 @@
+import './map.scss';
+import { ReactComponent as Map } from "@/assets/southKoreaLow.svg"
+
+const Mapimg = () => {
+  return (
+    <div className='mapbackground'>
+        {/* eslint-disable-next-line */}
+        <Map/>
+    </div>
+  )
+};
+
+export default Mapimg; 
