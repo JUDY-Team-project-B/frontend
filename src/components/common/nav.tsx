1ac848b42782e599df49bb171a311d88d5a955ef
BellYun <95991290+BellYun@users.noreply.github.com>
2023-05-15 16:31:47 +0900

    feat : nav및 메인 페이지 레이아웃 설정

---
+++ src/components/common/nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './nav.scss';
+
+const Nav = () => {
+  return (
+    <div className="navBackground">
+      <div className="gotomain">HANG OUT</div>
+      <div className="rightcomponents">
+        <div className="searchbar">searchbar</div>
+        <div className="login">LOGIN</div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/components/main/contentpage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './contentpage.scss';
+
+const Contentpage = () => {
+  return (
+    <div className="mapPageBackground">
+      <div>여행가고 싶은곳을 눌러보세요</div>
+      <div>mappage</div>
+    </div>
+  );
+};
+
+export default Contentpage;
