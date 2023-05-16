da61c273eefcfa120a2fb92ce345bec6f78bc231
BellYun <whd3558@naver.com>
2023-05-16 09:04:27 +0900

    컴포넌트 이름 수정 및 재구성

---
+++ src/components/common/preview/preview.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './preview.scss';
+
+const Preview = () => {
+  return (
+    <div className='content'> 
+        <div className='img'>
+          <div className='destiantion'> 
+            제주
+          </div> 
+          <div className='name'> 
+            하트 
+          </div> 
+        </div> 
+        <div> 
+          조회수
+        </div> 
+        <div> 
+          <div className='title'> 
+            title
+          </div> 
+          <div className='date'> 
+            date 
+          </div> 
+          <div className='how'> 
+            how 
+          </div> 
+          <div> 
+            need 
+          </div>        
+        </div> 
+      </div> 
+  );
+};
+
+export default Preview;
