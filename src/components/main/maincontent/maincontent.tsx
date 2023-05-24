76bf40297241d9543d0a2029d3a9aceb83008fc0
BellYun <whd3558@naver.com>
2023-05-24 16:41:05 +0900

    api 응답 위치 수정

--- src/components/main/maincontent/maincontent.tsx
+++ src/components/main/maincontent/maincontent.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './maincontent.scss';
 import Preview from '@/components/common/preview/preview';
-import { useQuery } from '@tanstack/react-query';
-import { restFetcher } from '@/queryClient';
+
 
 
 const Maincontent = () => {
 
-  const{ data, isLoading, isError, error} = useQuery(['POST'],()=> 
-    restFetcher({
-      method: 'GET',
-      path: `/api/v1/post/all`,
-    }),
-  );
-console.log(data);
 
   return (
     <div className="contentBackground">
