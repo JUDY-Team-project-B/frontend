f783aa38e7298aee39f84a64a50cf15fe1a4a295
BellYun <whd3558@naver.com>
2023-05-29 23:29:32 +0900

    Header요소 제작으로 인한 요소 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './index.scss';
 import Map from '@/components/main/map/map';
-import Nav from '@/components/common/nav/nav';
 import Maincontent from '@/components/main/maincontent/maincontent';
 
 const Main = () => {
   return (
     <div className="mainBackground">
-      <Nav/>
       <Map/>
       <Maincontent/>
     </div>
