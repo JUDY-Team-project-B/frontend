886f82586c79c6de94ef89bb766b7cf5467dd8a8
BellYun <whd3558@naver.com>
2023-05-31 16:13:11 +0900

    grid 레이아웃 세부 요소 수정

--- src/components/main/preview/preview.tsx
+++ src/components/main/preview/preview.tsx
@@ -4,6 +4,9 @@ import { useQuery } from '@tanstack/react-query';
 import { restFetcher } from '@/queryClient';
 import { postInform } from '@/mocks/handlers';
 import HeartOn from '@/assets/image/HeartOn.png'
+import View from '@/assets/image/detailView.png'
+import Heart from '@/assets/image/detailHeart.png'
+import Comment from '@/assets/image/detailcomment.png'
 
 const Preview = () => {
 
@@ -31,32 +34,44 @@ const Preview = () => {
 
           {data?.map((datas: postInform, index:any) =>(
             <div className='content' key={index}>
+
+
               <div className='img'>
                 <div className='imginfo'>
-
                   <div className='destination'>
-                    <div className='destinationtext'>
+                    <div className='destionationtext'>
                       {datas.travel_at}
                     </div>
+                  </div>
+                  <div className='heartlayout'>
                     <img src={HeartOn}></img>
                   </div>
                 </div>
               </div>
-            <div>
-              조회수
-            </div>
-            <div className='title'>
-              제목
-            </div>
-            <div className='data'>
-              날짜
-            </div>
-            <div className='title'>
-              {datas.travel_age}{datas.travel_gender}
-            </div>
-            <div className='need'>
-              {datas.travel_member}
-            </div>
+              <div className='detail'>
+                  78
+                  <img src={View}></img>
+                  7
+                  <img src={Comment}></img>
+                  14
+                  <img src={Heart}></img>
+              </div>
+              <div className='postinfo'>
+                 <div className='title'>
+                  {datas.post_title}
+                </div>
+                <div className='date'>
+                  날짜
+                </div>
+                <div className='date'>
+                  {datas.travel_age}{datas.travel_gender}
+                </div>
+                <div className='date'>
+                  {datas.travel_member}인 모집중
+                </div>
+              </div>
+
+
         </div>
         ))}
       </div>
