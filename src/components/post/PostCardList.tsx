20fc99ab5b7dd5d8c44f06d470cce9d1abec4d96
Kim-Hye-Lin <k1h2fls@gmail.com>
2023-06-04 14:33:29 +0900

    feat: image upload를 위한 기본 설정 세팅

--- src/components/post/PostCardList.tsx
+++ src/components/post/PostCardList.tsx
@@ -7,11 +7,11 @@ import TravelGuestSelect from './TravelGuestSelect';
 export const PostCardList = () => {
   return (
     <div>
-      <PostCard title="여행 지역">
-        <TravelRegionSelect />
-      </PostCard>
       <PostCard title="여행 기간">
         <TravelDateRangePicker />
+      </PostCard>
+      <PostCard title="여행 지역">
+        <TravelRegionSelect />
       </PostCard>
       <PostCard title="여행 인원">
         <TravelGuestSelect />
