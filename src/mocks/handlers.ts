26f4c966a80c43d2009d36c2cf936636d102a93b
BellYun <whd3558@naver.com>
2023-06-20 00:14:43 +0900

    여행 지역 설정 도 구간 스타일링

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,6 +10,64 @@ export interface postInform {
   travel_member: number;
 }
 
+export interface destination{
+  destination: string;
+}
+
+const destinationlist: destination[] = [
+  {
+    destination: '서울특별시'
+  },
+  {
+    destination:'부산광역시'
+  },
+  {
+    destination: '대구광역시'
+  },
+  {
+    destination:'인천광역시'
+  },
+  {
+    destination: '광주광역시'
+  },
+  {
+    destination:'대전광역시'
+  },
+  {
+    destination: '울산광역시'
+  },
+  {
+    destination:'세종시'
+  },
+  {
+    destination: '경기도'
+  },
+  {
+    destination:'강원도'
+  },
+  {
+    destination: '충청북도'
+  },
+  {
+    destination:'충청남도'
+  },
+  {
+    destination: '전라북도'
+  },
+  {
+    destination:'전라남도'
+  },
+  {
+    destination: '경상북도'
+  },
+  {
+    destination:'경상남도'
+  },
+  {
+    destination: '제주도'
+  },
+]
+
 
 const postList: postInform[] = [
   {
@@ -158,10 +216,18 @@ const postList: postInform[] = [
   },
 ];
 
+// export const handlers = [
+//   rest.get('/api/v1/post/all', (req, res, ctx) => {
+//     console.log('호출됨');
+//     console.log(postList);
+//     return res(ctx.status(200), ctx.json(postList));
+//   }),
+// ];
+
 export const handlers = [
-  rest.get('/api/v1/post/all', (req, res, ctx) => {
+  rest.get('/api/v1/post/list', (req, res, ctx) => {
     console.log('호출됨');
-    console.log(postList);
-    return res(ctx.status(200), ctx.json(postList));
+    console.log(destinationlist);
+    return res(ctx.status(200), ctx.json(destinationlist));
   }),
-];
+]
