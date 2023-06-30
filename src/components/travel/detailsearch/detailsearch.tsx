fb3170fa8fa6a928345cc50fb02ef268b1c0bba2
BellYun <whd3558@naver.com>
2023-06-30 19:00:22 +0900

    DatePicker적용

--- src/components/travel/detailsearch/detailsearch.tsx
+++ src/components/travel/detailsearch/detailsearch.tsx
@@ -4,10 +4,21 @@ import SearchIcon from '@/assets/image/Search icon.png'
 import './detailsearch.scss'
 import Destinationmodal from '../destinationmodal/destinationmodal';
 import Datemodal from '../datamodal/datemodal';
+import DatePicker from "react-datepicker";
+import { ko } from 'date-fns/esm/locale';
 
 const DetailSearch = () => {
+
+
     const [destinationmodal,setDestinationModal] = useState(false);
     const [datemodal,setDateModal] = useState(false);
+    const [startDate, setStartDate] = useState<any>(new Date());
+    const [endDate, setEndDate] = useState<any>(new Date());
+    const data = () =>{
+    return (
+      <DatePicker className='datemodallayout' selected={startDate} onChange={(date) => setStartDate(date)} />
+    );
+    }
   return (
     <div className='detailsearchlayout'>
         <button className='detailclicklayout' onClick={()=> destinationmodal ? setDestinationModal(false) :setDestinationModal(true)}>
@@ -16,12 +27,21 @@ const DetailSearch = () => {
         <div className='modaltest'>
             {destinationmodal === true? <Destinationmodal />:null}
         </div>
-        <button className='detailclicklayout' onClick={ ()=> datemodal ? setDateModal(false):setDateModal(true)}>
-            여행 기간
+        <button className='dateclicklayout'>
+            여행 기간 <DatePicker 
+             className='dataPickerLayout'
+             selected={startDate} 
+             locale={ko}
+             dateFormat="yyyy-MM-dd"
+             onChange={(date) => setStartDate(date)} />
+             
+             <DatePicker
+             className='dataPickerLayout'
+             selected={endDate} 
+             locale={ko}
+             dateFormat="yyyy-MM-dd"
+             onChange={(date) => setEndDate(date)} />
         </button>
-        <div className='modaltest'>
-            {datemodal === true? <Datemodal />:null}
-        </div>
         <div className='detailinputlayout'>
             <input placeholder='여행을 찾아보세요' className='input'/>
             <button>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -14,59 +14,59 @@ export interface destination{
   destination: string;
 }
 
-// const destinationlist: destination[] = [
-//   {
-//     destination: '서울특별시'
-//   },
-//   {
-//     destination:'부산광역시'
-//   },
-//   {
-//     destination: '대구광역시'
-//   },
-//   {
-//     destination:'인천광역시'
-//   },
-//   {
-//     destination: '광주광역시'
-//   },
-//   {
-//     destination:'대전광역시'
-//   },
-//   {
-//     destination: '울산광역시'
-//   },
-//   {
-//     destination:'세종시'
-//   },
-//   {
-//     destination: '경기도'
-//   },
-//   {
-//     destination:'강원도'
-//   },
-//   {
-//     destination: '충청북도'
-//   },
-//   {
-//     destination:'충청남도'
-//   },
-//   {
-//     destination: '전라북도'
-//   },
-//   {
-//     destination:'전라남도'
-//   },
-//   {
-//     destination: '경상북도'
-//   },
-//   {
-//     destination:'경상남도'
-//   },
-//   {
-//     destination: '제주도'
-//   },
-// ]
+const destinationlist: destination[] = [
+  {
+    destination: '서울특별시'
+  },
+  {
+    destination:'부산광역시'
+  },
+  {
+    destination: '대구광역시'
+  },
+  {
+    destination:'인천광역시'
+  },
+  {
+    destination: '광주광역시'
+  },
+  {
+    destination:'대전광역시'
+  },
+  {
+    destination: '울산광역시'
+  },
+  {
+    destination:'세종시'
+  },
+  {
+    destination: '경기도'
+  },
+  {
+    destination:'강원도'
+  },
+  {
+    destination: '충청북도'
+  },
+  {
+    destination:'충청남도'
+  },
+  {
+    destination: '전라북도'
+  },
+  {
+    destination:'전라남도'
+  },
+  {
+    destination: '경상북도'
+  },
+  {
+    destination:'경상남도'
+  },
+  {
+    destination: '제주도'
+  },
+]
 
 
 const postList: postInform[] = [
@@ -216,18 +216,18 @@ const postList: postInform[] = [
   },
 ];
 
-export const handlers = [
-  rest.get('/api/v1/post/all/1', (req, res, ctx) => {
-    console.log('호출됨');
-    console.log(postList);
-    return res(ctx.status(200), ctx.json(postList));
-  }),
-];
-
 // export const handlers = [
-//   rest.get('/api/v1/post/list', (req, res, ctx) => {
+//   rest.get('/api/v1/post/all/1', (req, res, ctx) => {
 //     console.log('호출됨');
-//     console.log(destinationlist);
-//     return res(ctx.status(200), ctx.json(destinationlist));
+//     console.log(postList);
+//     return res(ctx.status(200), ctx.json(postList));
 //   }),
-// ]
+// ];
+
+export const handlers = [
+  rest.get('/api/v1/post/list', (req, res, ctx) => {
+    console.log('호출됨');
+    console.log(destinationlist);
+    return res(ctx.status(200), ctx.json(destinationlist));
+  }),
+]
