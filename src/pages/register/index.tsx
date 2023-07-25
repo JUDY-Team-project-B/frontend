edd875eb5bee74abf66a9112eef7a4aee4bf9cf3
이준희 <dev.junhui@gmail.com>
2023-07-26 03:47:04 +0900

    FEAT : 회원가입 유효성 검사 적용

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import '../../assets/font/font.css';
+import axios from 'axios';
 
 const SignUpContainer = styled.div`
   display: flex;
@@ -58,7 +59,7 @@ const Input = styled.input`
   margin-bottom: 10px;
   padding: 0.9rem;
   width: 25rem;
-  border: 1px solid #ccc;
+  border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
   border-radius: 5px;
 `;
 
@@ -85,15 +86,90 @@ const SignUpPage: React.FC = () => {
   const [nickname, setNickname] = useState('');
   const [gender, setGender] = useState('');
   const [age, setAge] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+  const [isValidPasswordCheck, setIsValidPasswordCheck] = useState(true);
+  const [isValidNickname, setIsValidNickname] = useState(true);
+  const [isValidGender, setIsValidGender] = useState(true);
+  const [isValidAge, setIsValidAge] = useState(true);
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
   const handleSignUp = (e: React.FormEvent) => {
     e.preventDefault();
+
+    //이메일 주소 유효성 검사
+    if (!emailRegex.test(username)) {
+      setIsValidEmail(false);
+      alert('유효한 이메일 주소를 입력해주세요.');
+      return;
+    }
+    // 비밀번호 유효성 검사
+    if (!passwordRegex.test(password)) {
+      alert('영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.');
+      setIsValidPassword(false);
+      return;
+    }
+
+    if (password !== passwordCheck) {
+      setIsValidPasswordCheck(false);
+      alert('비밀번호 값이 일치하지 않습니다.');
+      return;
+    }
+
+    if (nickname.length < 2 || nickname.length > 10) {
+      setIsValidNickname(false);
+      alert('닉네임은 2 ~ 10글자 사이어야 합니다.');
+      return;
+    }
+
+    if (!gender) {
+      setIsValidGender(false);
+      alert('성별을 선택해주세요.');
+      return;
+    }
+
+    // 나이 선택 여부 검사
+    if (!age) {
+      setIsValidAge(false);
+      alert('나이를 선택해주세요.');
+      return;
+    }
+
+    // 유효성 검사에 통과하면 isValid를 true로 설정
+    setIsValidEmail(true);
+    setIsValidPassword(true);
+    setIsValidPasswordCheck(true);
+    setIsValidNickname(true);
+    setIsValidGender(true);
+    setIsValidAge(true);
+
     console.log('Username:', username);
     console.log('Password:', password);
     console.log('PasswordCheck:', passwordCheck);
     console.log('Nickname:', nickname);
     console.log('Gender:', gender);
     console.log('Age:', age);
+
+    const sendData = {
+      email: username,
+      password: password,
+      nickname: nickname,
+      gender: gender,
+      age: age,
+    };
+    //회원가입
+    axios
+      .post('http://localhost:8080/api/v1/auth/signup', sendData)
+      .then((response) => {
+        // 회원가입 성공
+        alert('회원가입에 성공하였습니다!');
+        // navigate('/main');
+      })
+      .catch((error) => {
+        console.log(실패);
+      });
   };
 
   return (
@@ -106,6 +182,7 @@ const SignUpPage: React.FC = () => {
           placeholder="이메일"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          invalid={!isValidEmail}
         />
         <InputTitle>비밀번호</InputTitle>
         <InputTitle
@@ -118,6 +195,7 @@ const SignUpPage: React.FC = () => {
           placeholder="비밀번호"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          invalid={!isValidPassword}
         />
         <InputTitle>비밀번호 확인</InputTitle>
         <Input
@@ -125,27 +203,37 @@ const SignUpPage: React.FC = () => {
           placeholder="비밀번호 확인"
           value={passwordCheck}
           onChange={(e) => setPasswordCheck(e.target.value)}
+          invalid={!isValidPasswordCheck}
         />
         <InputTitle>닉네임</InputTitle>
         <InputTitle
           style={{ color: '#a3a5a5', marginTop: '0.5rem', fontWeight: '100' }}
         >
-          다른 유저와 겹치지 않도록 입력해주세요. (2~15자)
+          다른 유저와 겹치지 않도록 입력해주세요. (2~10자)
         </InputTitle>
         <Input
           type="text"
-          placeholder="닉네임 (2~15자)"
+          placeholder="닉네임 (2~10자)"
           value={nickname}
           onChange={(e) => setNickname(e.target.value)}
+          invalid={!isValidNickname}
         />
         <InputTitle>회원정보</InputTitle>
-        <Select value={gender} onChange={(e) => setGender(e.target.value)}>
+        <Select
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          invalid={!isValidGender}
+        >
           <Option value="">성별</Option>
           <Option value="남자">남자</Option>
           <Option value="여자">여자</Option>
         </Select>
 
-        <Select value={age} onChange={(e) => setAge(e.target.value)}>
+        <Select
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          invalid={!isValidAge}
+        >
           <Option value="">나이</Option>
           <Option value="10대">10대</Option>
           <Option value="20대">20대</Option>
