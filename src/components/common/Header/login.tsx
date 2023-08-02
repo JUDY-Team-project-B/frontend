ff6965dbcad0755f327c7534489ebba09de97859
이준희 <dev.junhui@gmail.com>
2023-08-02 17:18:22 +0900

    FEAT : NAV 바 위치 및 디자인 수정

--- src/components/common/Header/login.tsx
+++ src/components/common/Header/login.tsx
@@ -4,67 +4,112 @@ import { useRecoilState } from 'recoil';
 import { UUid, User } from '@/atom/atom';
 import { useNavigate } from 'react-router-dom';
 
+export const Login = () => {
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false); // State variable for login modal
+  const [userData, setUserData] = useRecoilState<User>(UUid);
+  const [isLogin, setisLogin] = useState<boolean>(false);
 
+  const navigate = useNavigate();
+  useEffect(() => {
+    setisLogin(userData.is_active);
+    console.log(userData.is_active);
+  });
 
-export const Login = () =>{
-    const [isLoginModalOpen, setLoginModalOpen] = useState(false); // State variable for login modal
-    const [userData, setUserData] = useRecoilState<User>(UUid);
-    const [isLogin, setisLogin] = useState<boolean>(false);
+  const openLoginModal = () => {
+    setLoginModalOpen(true); // Open the login modal
+  };
 
-    const navigate = useNavigate();
-    useEffect(()=>{
-      setisLogin(userData.is_active)
-      console.log(userData.is_active)
-    })
+  const closeLoginModal = () => {
+    setLoginModalOpen(false); // Close the login modal
+  };
 
-    const openLoginModal = () => {
-      setLoginModalOpen(true); // Open the login modal
-    };
-  
-    const closeLoginModal = () => {
-      setLoginModalOpen(false); // Close the login modal
-    };
+  const logout = () => {
+    console.log('로그아웃');
+    setUserData({
+      is_active: false,
+    });
+    localStorage.removeItem('accessToken');
+    navigate('/');
+  };
 
-    const logout = () => {
-      console.log('로그아웃')
-      setUserData({
-        is_active:false
-      });
-      localStorage.removeItem('accessToken')
-      navigate('/');
-    }
+  const gotoMypage = () => {
+    navigate('/mypage');
+  };
 
-    const gotoMypage = ()=>{
-      navigate('/mypage')
-    }
-
-    const gotoRegister = () =>{
-      navigate('/register')
-    }
-    if (isLogin === false) {
-        return (
-          <div>
-            <button onClick={openLoginModal}>
-              로그인
-            </button>
-            /
-            <button onClick={gotoRegister}>
-              회원가입
-            </button>
-            {isLoginModalOpen && (<LoginModal open={isLoginModalOpen} onClose={closeLoginModal} />)}
-          </div> 
-        );
-      } else {
-        return (
-          <div>
-            <button onClick={gotoMypage}>
-              내정보
-            </button>
-            /
-            <button onClick={logout}>
-              로그아웃
-            </button>
-          </div>
-        )
-      }
-}
+  const gotoRegister = () => {
+    navigate('/register');
+  };
+  if (isLogin === false) {
+    return (
+      <div style={{ marginLeft: '15rem', display: 'flex' }}>
+        <div
+          style={{
+            display: 'flex',
+            width: '5.5rem',
+            justifyContent: 'center',
+            padding: '.5rem',
+            backgroundColor: '#4ab8f7',
+            color: 'white',
+            borderRadius: '1rem',
+            fontFamily: 'NanumSquareNeo-Variable',
+          }}
+        >
+          <button onClick={openLoginModal}>로그인</button>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '6.5rem',
+            justifyContent: 'center',
+            padding: '.5rem',
+            backgroundColor: '#4ab8f7',
+            color: 'white',
+            borderRadius: '1rem',
+            marginLeft: '1rem',
+            fontFamily: 'NanumSquareNeo-Variable',
+          }}
+        >
+          <button onClick={gotoRegister}>회원가입</button>
+          {isLoginModalOpen && (
+            <LoginModal open={isLoginModalOpen} onClose={closeLoginModal} />
+          )}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ marginLeft: '14rem', display: 'flex' }}>
+        <div
+          style={{
+            display: 'flex',
+            width: '5.5rem',
+            justifyContent: 'center',
+            padding: '.5rem',
+            backgroundColor: '#4ab8f7',
+            color: 'white',
+            borderRadius: '1rem',
+            marginLeft: '1rem',
+            fontFamily: 'NanumSquareNeo-Variable',
+          }}
+        >
+          <button onClick={gotoMypage}>내정보</button>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '6.5rem',
+            justifyContent: 'center',
+            padding: '.5rem',
+            backgroundColor: '#4ab8f7',
+            color: 'white',
+            borderRadius: '1rem',
+            marginLeft: '1rem',
+            fontFamily: 'NanumSquareNeo-Variable',
+          }}
+        >
+          <button onClick={logout}>로그아웃</button>
+        </div>
+      </div>
+    );
+  }
+};
