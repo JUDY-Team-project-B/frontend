70b1807dca30a2e1fdb013be1f4ee53e70c3b188
BellYun <95991290+BellYun@users.noreply.github.com>
2023-09-07 10:19:08 +0900

    refactor : 재수정

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -13,32 +13,33 @@ export const Header = () => {
   const [userData, setUserData] = useRecoilState<User>(UUid);
   const [keyword, setKeyword] = useState<any>(null);
 
-  const onSilentRefresh = async () =>{
-    try{
-      const response = await axios.post('http://localhost:8080/api/v1/auth/refresh-token');
-      const accessToken  = response.data.data.accessToken;
-      console.log(accessToken)
+  const onSilentRefresh = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/api/v1/auth/refresh-token',
+      );
+      const accessToken = response.data.data.accessToken;
+      console.log(accessToken);
       axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-    }catch(e){
-      alert('다시 로그인해주세요')
+    } catch (e) {
+      alert('다시 로그인해주세요');
       navigate('/');
-      
     }
-  }
+  };
 
   const gotoMain = () => {
     navigate('/');
     location.reload();
   };
 
-  const IsLogin = () =>{
-    const Token = cookie.load('accessTokens')
-    if(Token === undefined){
-      setisLogin(false)
-    }else{
-      setisLogin(true)
+  const IsLogin = () => {
+    const Token = cookie.load('accessTokens');
+    if (Token === undefined) {
+      setisLogin(false);
+    } else {
+      setisLogin(true);
     }
-  }
+  };
 
   useEffect(() => {
     IsLogin();
@@ -46,13 +47,13 @@ export const Header = () => {
   });
 
   const search = (): void => {
-    if(keyword === null){
-      alert('검색어를 입력해주세요')
-    }else{
+    if (keyword === null) {
+      alert('검색어를 입력해주세요');
+    } else {
       const queryParems1 = new URLSearchParams();
       const queryParems2 = new URLSearchParams();
       queryParems1.set('q', keyword);
-      queryParems2.set('t',Selected);
+      queryParems2.set('t', Selected);
       const queryString1 = queryParems1.toString();
       const queryString2 = queryParems2.toString();
       navigate(`/travel?${queryString1}&${queryString2}`);
@@ -67,27 +68,31 @@ export const Header = () => {
 
   const gotoWrite = () => {
     navigate('/create-post');
-    
   };
 
-  const [Selected, setSelected] = useState("title");
-  const selectList = [{
-    korea:"제목",
-    eng:"title"
-  }, {
-    korea:"내용",
-    eng:"content"
-  },{
-    korea:"닉네임",
-    eng:"nickname"
-  },{
-    korea:"지역",
-    eng:"stateAndCity"
-  }];
+  const [Selected, setSelected] = useState('title');
+  const selectList = [
+    {
+      korea: '제목',
+      eng: 'title',
+    },
+    {
+      korea: '내용',
+      eng: 'content',
+    },
+    {
+      korea: '닉네임',
+      eng: 'nickname',
+    },
+    {
+      korea: '지역',
+      eng: 'stateAndCity',
+    },
+  ];
 
-  const handleSelect = (e:any) => {
+  const handleSelect = (e: any) => {
     setSelected(e.target.value);
-    console.log(Selected)
+    console.log(Selected);
   };
 
   return (
@@ -102,15 +107,13 @@ export const Header = () => {
             >
               HANG OUT
             </button>
-            <button onClick={gotoWrite}>
-              떠나기
-            </button >
+            <button onClick={gotoWrite}>떠나기</button>
           </div>
         </div>
         <div className="leftdiv">
           <div className="inputlayout">
             <select onChange={handleSelect} value={Selected}>
-              {selectList.map((item)=>(
+              {selectList.map((item) => (
                 <option value={item.eng} key={item.eng}>
                   {item.korea}
                 </option>
@@ -122,7 +125,9 @@ export const Header = () => {
               onChange={setWord}
               value={keyword}
             />
-            <button className='buttonDisign' onClick={search}>검색</button>
+            <button className="buttonDisign" onClick={search}>
+              검색
+            </button>
           </div>
           <Login />
         </div>
