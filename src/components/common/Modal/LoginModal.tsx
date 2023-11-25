5497b1af2ceb0e4a96ec56d979041efa0e16cf52
BellYun <95991290+BellYun@users.noreply.github.com>
2023-11-25 22:06:46 +0900

    fix : oauth2

--- src/components/common/Modal/LoginModal.tsx
+++ src/components/common/Modal/LoginModal.tsx
@@ -71,10 +71,43 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
     }
   };
 
-  const handleGoogleLogin = () => {
-    console.log('google login 이동');
-    location.href= BASE_URL + '/auth/oauth2/authorize/google?redirect-uri=' + BASE_HOST;
-  };
+  21
+  function getGoogleLoginUrl() {
+      // Google's OAuth 2.0 endpoint for requesting an access token
+      var oauth2Endpoint = BASE_URL+'auth/oauth2/authorize/google';
+    
+      // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+      var form = document.createElement('form');
+      form.setAttribute('method', 'GET'); // Send as a GET request.
+      form.setAttribute('action', oauth2Endpoint);
+      
+    
+      // // // Parameters to pass to OAuth 2.0 endpoint.
+      var params = {'redirect-uri': BASE_HOST};
+  
+      // // Add form parameters as hidden input values.
+      for (var p in params) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+      }
+    
+      // Add form to page and submit it to open the OAuth 2.0 endpoint.
+      document.body.appendChild(form);
+      form.submit();
+    }
+    
+    const handleGoogleLogin = async () => {
+      console.log('google login 이동');
+      try{
+        getGoogleLoginUrl();
+  
+      } catch (error) {
+        console.error('Error adding like:', error);
+      }
+    }
   return (
     <Dialog
       open={open}
